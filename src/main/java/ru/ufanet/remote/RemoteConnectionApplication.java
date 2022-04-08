30559ac18643370c93ddef6677a981464fe9f2a9
ALLABER <ubermenschalone@gmail.com>
2022-04-08 18:09:24 +0500

    Initial commit

---
+++ src/main/java/ru/ufanet/remote/RemoteConnectionApplication.java
@@ -0,0 +1,13 @@
+package ru.ufanet.remote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteConnectionApplication.class, args);
+	}
+
+}
